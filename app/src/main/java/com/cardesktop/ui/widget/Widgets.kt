a0f317f53ad5ef9262a559d468c6f9ea07220f85
huangpeng <275945793@qq.com>
2026-06-10 14:38:08 +0800

    pd

--- app/src/main/java/com/cardesktop/ui/widget/Widgets.kt
+++ app/src/main/java/com/cardesktop/ui/widget/Widgets.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextUnit
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cardesktop.ui.theme.*
