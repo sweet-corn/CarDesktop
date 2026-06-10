f51489ac67951f34ec780091e5b628501f21e652
huangpeng <275945793@qq.com>
2026-06-10 12:35:08 +0800

    upd

--- app/src/main/java/com/cardesktop/ui/widget/Widgets.kt
+++ app/src/main/java/com/cardesktop/ui/widget/Widgets.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cardesktop.ui.theme.*
@@ -56,7 +57,7 @@ fun TopStatusBar(
 }
 
 @Composable
-private fun StatusIcon(icon: String, size: sp) {
+private fun StatusIcon(icon: String, size: TextUnit) {
     Text(text = icon, fontSize = size, color = Color.White)
 }
 
